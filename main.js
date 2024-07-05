de1a9cf2703b6be0edf92c149f8808e447dbe0e8
Zaky <145343615+ZAKYVANGOBEL15@users.noreply.github.com>
2024-07-05 19:25:59 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,6 @@
-
-let hours = new Date().getHours() 
-
-if(hours < 10) {
-	hours = 'Selamat Pagi'
-}else if(hours > 17 ){
-	hours = 'Selamat Malam'
-}else {
-	hours = 'Selamat Siang'
-}
-
-// alert(hours)
-
-document.getElementById('toggle1').onclick = function() {
-
-	let ul2 = document.getElementById('demo2')
+function myfunction() {
+    let sidebar = document.getElementById('demo')
 
 
-	ul2.classList.toggle('ul3') 
-
-
-
-}
-
-document.getElementById('mode1').onclick = function() {
-
-	let mode = document.getElementById('bulat1')
-
-	mode.classList.toggle('bulat2')
-
-
-	let body = document.getElementById('cont_demo')
-
-	body.classList.toggle('darkmode1')
-
-	
-
-}
-
-document.getElementById('mode2').onclick = function() {
-
-	let mode = document.getElementById('bulat2')
-
-	mode.classList.toggle('bulat3')
-
-
-	let body = document.getElementById('cont_demo')
-
-	body.classList.toggle('darkmode2')
-
-	
-
+    sidebar.classList.toggle('sidebar2')
 }
