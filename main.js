ac3e34d44df2ca07f50091843cadfede5e33b510
Zaky <145343615+ZAKYVANGOBEL15@users.noreply.github.com>
2024-09-28 15:01:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let toggle = document.getElementById('toggle');
+
+let sidebar = document.getElementById('sidebar');
+
+
+
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle('sidebar2');
+})
